b84540d7d2bf8447d1972228549d0326e48c6c8d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-06 11:00:08 +0530

    adding ArrowFromBottomView class

--- arrowfrombottomview/src/main/java/com/anwesh/uiprojects/arrowfrombottomview/ArrowFromBottomView.kt
+++ arrowfrombottomview/src/main/java/com/anwesh/uiprojects/arrowfrombottomview/ArrowFromBottomView.kt
@@ -52,3 +52,21 @@ fun Canvas.drawAFBNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class ArrowFromBottomView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
