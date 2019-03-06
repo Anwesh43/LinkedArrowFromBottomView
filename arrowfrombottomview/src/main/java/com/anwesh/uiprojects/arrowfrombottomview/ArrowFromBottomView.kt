4efadaa0fb653dece3c80c2a975b5ac16edeb8b0
Anwesh43 <anweshthecool0@gmail.com>
2019-03-06 10:49:20 +0530

    adding constants and importing libraries

--- arrowfrombottomview/src/main/java/com/anwesh/uiprojects/arrowfrombottomview/ArrowFromBottomView.kt
+++ arrowfrombottomview/src/main/java/com/anwesh/uiprojects/arrowfrombottomview/ArrowFromBottomView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.arrowfrombottomview
 /**
  * Created by anweshmishra on 06/03/19.
  */
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#212121")
