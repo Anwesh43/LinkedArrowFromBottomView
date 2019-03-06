6efbca764e97b37c970b03495f5720b6065b105d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-06 11:08:35 +0530

    adding AFBNode class

--- arrowfrombottomview/src/main/java/com/anwesh/uiprojects/arrowfrombottomview/ArrowFromBottomView.kt
+++ arrowfrombottomview/src/main/java/com/anwesh/uiprojects/arrowfrombottomview/ArrowFromBottomView.kt
@@ -117,4 +117,48 @@ class ArrowFromBottomView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AFBNode(var i : Int, val state : State = State()) {
+
+        private var next : AFBNode? = null
+        private var prev : AFBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = AFBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAFBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AFBNode {
+            var curr : AFBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
